a80685a043133aa68464a0d944fa0ac579edab6b
ylxdzsw <ylxdzsw@gmail.com>
2014-08-22 15:33:19 +0800

    plot4 done

---
+++ plot4.R
@@ -0,0 +1,9 @@
+plot4 <- function(NEI, SCC){
+  PMfromCoal <- NEI[unclass(SCC[match(NEI$SCC,SCC$SCC),"EI.Sector"]) %in% c(13,18,23),]
+  PMayearfromCoal <- tapply(PMfromCoal$Emissions,PMfromCoal$year,sum)
+  
+  png(filename="plot4.png",width=480,height=480)
+  plot(names(PMayear),PMayearfromCoal,type="b",xlab="Year",ylab="PM2.5")
+  title("PM2.5 each year that from Coal")
+  dev.off()
+}
